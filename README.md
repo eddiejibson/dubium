30b73e43923d04b6a8546690cbf77786c4c240d6
Eddie Jibson <jibson@tuta.io>
2019-03-31 20:56:28 +0100

    v0.0.2

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 <div align="center">
     <img src="icon.png">
     <br>
-    <p>Light Javascript date and time utilities (formatting, ranges e.t.c) with zero dependencies.</p>
+    <p>Light Javascript date and time utilities (formatting, ranges e.t.c).</p>
 </div>
---
+++ babel.config.js
@@ -0,0 +1,26 @@
+const presets = [
+    ["minify"],
+    [
+        "@babel/env",
+        {
+            targets: {
+                edge: "15",
+                firefox: "50",
+                chrome: "50",
+                safari: "8",
+                ie: "10"
+            },
+            useBuiltIns: "entry",
+        },
+    ],
+];
+
+const plugins = [
+    ["@babel/plugin-proposal-class-properties"]
+]
+
+
+module.exports = {
+    presets,
+    plugins
+};
